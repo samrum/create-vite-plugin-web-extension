5bc6cbec7334b111566d5e473f1eb70c2a3082fc
Ruben Medina <dev@rubenmedina.com>
2022-02-05 23:41:43 -0800

    refactor: update vanilla templates to be closer to others

--- template/framework/vanilla/src/entries/popup/main.js
+++ template/framework/vanilla/src/entries/popup/main.js
@@ -5,6 +5,6 @@ const imageUrl = new URL(logo, import.meta.url).href;
 
 document.querySelector("#app").innerHTML = `
   <img src="${imageUrl}" height="45" />
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h1>Popup</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a>
 `;
