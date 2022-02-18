bc39cc9596a16680bd5d5726220e0f8d301f9511
Ruben Medina <dev@rubenmedina.com>
2022-02-17 22:00:36 -0800

    refactor: remove unnecessary import.meta.hot in manifest v2 bg script

--- template/config/manifestV2/src/entries/background/main.js
+++ template/config/manifestV2/src/entries/background/main.js
@@ -1,9 +1,5 @@
 import browser from "webextension-polyfill";
 
-if (import.meta.hot) {
-  // Enable HMR
-}
-
 browser.runtime.onInstalled.addListener(() => {
   console.log("Extension installed");
 });
