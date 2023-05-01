ed615f9a640211a6be6b18032d7bd404dcb7e5d8
Ruben Medina <dev@rubenmedina.com>
2023-04-30 22:50:53 -0700

    chore: add script to run commands on playground directories
    also optimize build-playground script a bit

---
+++ scripts/run-playground.mjs
@@ -0,0 +1,42 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { spawn } from "child_process";
+import { readdir } from "fs/promises";
+
+async function getDirectories(path) {
+  try {
+    return (await readdir(path, { withFileTypes: true }))
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => dirent.name);
+  } catch (e) {
+    return [];
+  }
+}
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const playgroundDir = join(__dirname, "../playground");
+const builds = await getDirectories(playgroundDir);
+
+const supportedCommands = ['build', 'dev', 'install'];
+
+const inputCommand = process.argv[2] ?? 'build';
+
+if (!supportedCommands.includes(inputCommand)) {
+  throw new Error(`Supported commands: ${supportedCommands.join(', ')}`);
+}
+
+let pnpmCommand = 'build';
+switch (inputCommand) {
+  case 'build':
+    pnpmCommand = 'build --mode development';
+    break;
+  case 'dev':
+    pnpmCommand = 'dev --clearScreen false';
+    break;
+  default: 
+    pnpmCommand = inputCommand;
+}
+
+for (const build of builds) {
+  spawn(`cd ${join(playgroundDir, build)} && pnpm ${pnpmCommand}`, { stdio: 'inherit', shell: true });
+}
